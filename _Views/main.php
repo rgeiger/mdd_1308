33b856f99db258c8b98f1db460426a1e07d8b196
Robert Geiger <rjgeiger@me.com>
2013-08-21 18:10:16 -0500

    added main php view for display a small about section on the home page

---
+++ _Views/main.php
@@ -0,0 +1,16 @@
+<div class="row">
+	<div class="col-xs-12">
+		<h2 class="text-success">Welcome to Leading Light Star</h2>
+        
+    </div>
+</div><!--end row -->
+    
+    <?php 
+		$timestamp = time();
+		echo strftime("The date today is %m/%d/%y", $timestamp);			
+	?>
+    
+    <p class="well lead">This is a personality learning website with blog, facebook integration, community support
+       from other users, and many more features to come.  Think Facebook meets
+       <a href="http://www.myersbriggs.org">Briggs Meyers</a>.  Sign up to learn more..Its FREE.
+       <a href="_Views/signUp.htm"><button class="btn btn-success">Sign Up</button></a></p>
