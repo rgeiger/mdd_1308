682039582aa19b006175edbc7f14a0c60d9585fd
Robert Geiger <rjgeiger@me.com>
2013-08-29 12:59:29 -0500

    updated header view page

--- _Views/header.php
+++ _Views/header.php
@@ -1,39 +1,13 @@
-<!doctype html>
-<html>
 <head>
 <meta charset="UTF-8">
 <title>Leading Light Star</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" media="screen" 
-	href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-wip/css/bootstrap.min.css">
+<link rel="stylesheet" href="_Assets/bootstrap/css/bootstrap.min.css">
 <!-- Overide CSS for customizing bootstrap -->
+<link rel="stylesheet" href="_Assets/_css/jumbotron.css">
 <link rel="stylesheet" href="_Assets/_css/overide.css">
 </head>
 
-<body>
-	<div class="container">
-    	<div class="col-md-12">
-		<!--code starts here -->
-        <header>
-			<nav class="navbar" role="navigation">
-     		<div class="navbar-header">
-    		<button type="button" class="navbar-toggle" 
-            	data-toggle="collapse" 
-            	data-target=".navbar-ex1-collapse">
-        			<span class="sr-only">Toggle navigation</span>
-        			<span class="icon-bar"></span>
-        			<span class="icon-bar"></span>
-        			<span class="icon-bar"></span>
-    		</button>
-            
-            <img src="_Assets/_images/logo.png" alt="logo" class="img-circle pull-left">
-    		<a class="navbar-brand" href="?action=main">Leading Light Star</a>
-        	<a href='?action=login'>
-            	<button type="button" class="btn btn-primary navbar-btn">Log in</button>
-            </a>
-			</div><!--nav bar header -->
-    		</nav>
-		</header>
-        </div><!--end column -->
+
         
