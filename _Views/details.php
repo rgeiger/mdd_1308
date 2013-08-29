fad1e3c7d75c4ae927caee59ab8330565dc3fd6d
Robert Geiger <rjgeiger@me.com>
2013-08-29 12:57:17 -0500

    added details view page.. for viewing user profiles

--- _Views/details.php
+++ _Views/details.php
@@ -1,21 +1,14 @@
 <?php
 
 foreach($data as $d){
-    ?><ul class="unstyled">
-            
-        <li>
-            <?php echo " <strong>Email: </strong>";
-            echo $d["email"] ?>
-        </li>
-	<li>
-            <?php echo " <strong>Phone: </strong>";
-            echo $d["phone"]; ?>
-        </li>
-	<li>
-            <?php echo " <strong>Address </strong>";
-            echo $d["address"];
-            echo "<br>"; ?>
-     </ul>
-<?
+	echo " <strong>Email:</strong>";
+	echo $d["email"];
+	echo " <strong>Phone:</strong>";
+	echo $d["phone"];
+	echo " <strong>Address</strong>";
+	echo $d["address"];
+	echo "<br>";
+	
 }
+
 ?>
