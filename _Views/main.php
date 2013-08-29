a4c20578e7ca24be87c8f567e9a9bf112c25431a
Robert Geiger <rjgeiger@me.com>
2013-08-29 13:02:30 -0500

    main php view is the main view or default view in the middle body section of the home page .. it changes based on what the user does

--- _Views/main.php
+++ _Views/main.php
@@ -1,16 +1,12 @@
-<div class="row">
-	<div class="col-xs-12">
-		<h2 class="text-success">Welcome to Leading Light Star</h2>
+<div class="container">	
+	<div class="jumbotron">
+		<h1>Welcome to Leading Light Star</h1>
+		<p>This is a personality learning website with blog, facebook integration, community support
+from other users, and many more features to come.  Think Facebook meets
+			<a href="http://www.myersbriggs.org" target="briggsmyers">Briggs Myers</a>.  Sign up to learn more..Its
+			<span class="text-success">FREE.</span></p>    
+		<p><a href="?action=signUp">
+        	<button class="btn btn-primary btn-large">Sign Up &raquo;</button></a></p>
+	</div><!--closes jumbotron -->
+</div><!--closes container -->
         
-    </div>
-</div><!--end row -->
-    
-    <?php 
-		$timestamp = time();
-		echo strftime("The date today is %m/%d/%y", $timestamp);			
-	?>
-    
-    <p class="well lead">This is a personality learning website with blog, facebook integration, community support
-       from other users, and many more features to come.  Think Facebook meets
-       <a href="http://www.myersbriggs.org">Briggs Meyers</a>.  Sign up to learn more..Its FREE.
-       <a href="_Views/signUp.htm"><button class="btn btn-success">Sign Up</button></a></p>
