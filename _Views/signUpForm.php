09f63ed3d7dfdf6a04241ddb2523dabf47e5d4f4
Robert Geiger <rjgeiger@me.com>
2013-08-29 13:06:02 -0500

    sign up form added for users to sign up

---
+++ _Views/signUpForm.php
@@ -0,0 +1,16 @@
+<link href="_Assets/_css/signin.css" rel="stylesheet">
+    
+<div class="container">    
+	<form class="form-signin" role="form" action="?action=addaction" method="post">
+    <h2 class="form-signin-heading">Please sign up</h2>
+        	<input class="form-control" type ="text" name="un" placeholder="User" />
+        	<input class="form-control" type ="text" name="first" placeholder="First" />
+        	<input class="form-control" type ="text" name="last" placeholder="Last" />
+        	<input class="form-control" type ="text" name="pass" placeholder="Password" />
+        	<input class="form-control" type ="text" name="email" placeholder="Email" />
+        	
+            <button class="btn btn-large btn-success btn-block"
+            type="submit" value="Add">Go</button>
+	</form>
+</div>
+
