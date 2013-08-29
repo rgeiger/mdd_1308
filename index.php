109b0ad8b7c79075c2ff01e48a8e55754784c86e
Robert Geiger <rjgeiger@me.com>
2013-08-29 13:00:05 -0500

    added index.htm page for quick viewing of look and feel .. updated index. php file

--- index.php
+++ index.php
@@ -1,52 +1,50 @@
 <?php
 ob_start();
 session_start();
+
+require_once('_Controllers/functions.php');
+
 ?>
 
-<?php require_once ("_Models/viewModel.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php viewHeader(); ?>
+
 <?php include ("_Models/queryModel.php"); ?>
 
-<?php function redirect_to($new_location) {
-    header("Location: " . $new_location);
-    exit;
-} ?>
-
-<?php $pagename = "index"; ?>
-
-<?php $view = new viewModel();
-        $view->showHeader();
-?>
-
-<section class="jumbotron">
-	
-    
-    <?php		
+<body>
+    <?php
+		viewNav();
+			
 	$query = new queryModel();
 	
 	if(!empty($_GET["action"])){
-            
-        if($_GET["action"]=="home") {
 		
-		echo "please choose a username";
-	}
-           
         if($_GET["action"]=="login") {
-		
-		$view->getView("_Views/loginForm.php");
-		
+      
+		viewLoginForm();
 		$_POST["un"] = "";
 		$_POST["pass"] = "";
         }
-        if($_GET["action"]=="checklogin") {
 		
-		
-		$result = $query->checklogin($_POST["un"],$_POST["pass"]);	
+		if($_GET["action"]=="signUp") {
+			viewSignUpForm();
+        }
+		if($_GET["action"]=="addaction") {
+            $query->add($_POST["first"],$_POST["last"],$_POST["un"],$_POST["pass"],
+                    $_POST["email"]);
+            $result = $query->getAll();
 			
+            echo '<h2>Please login to view your page</h2>';
+        }
+		
+        if($_GET["action"]=="checklogin") {
+		$result = $query->checklogin($_POST["un"],$_POST["pass"]);	
 		if(count($result)>0){                    
-                    redirect_to("protected.php");
+                    redirect_to("userPage.php");
 		}else{
                     echo '<h3 class="alert-danger">Login Error</h3>';
-                    $view->getView("_views/loginform.php");
 		}		
 	}
 	if($_GET["action"]=="details") {
@@ -54,25 +52,22 @@ session_start();
 		$result = $query->getOne($_GET["id"]);
 		$view->getView("_Views/details.php", $result);		
 	}	
-        if($_GET["action"]=="logout") {
-				
-	}
-        
         
 }else{
-	
-	$view->getView("_Views/main.php");
+	viewMain();
 }
-			
-		?>
-
-
-
-
-	
-
-<?php $view = new viewModel();
-	$view->showFooter();
-        ob_flush()
 ?>
+
+<?php
+	viewInfo();
+	viewFooter();
+    ob_flush()
+?>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-wip/js/bootstrap.min.js"></script>
+<script src="_Libraries/_js/respond.min.js"></script>
+</body>
+</html>
  
