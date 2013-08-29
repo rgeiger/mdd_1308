5cd8eca068fc6a6b5bf04f5a9986c5eae1e50bc1
Robert Geiger <rjgeiger@me.com>
2013-08-29 13:01:54 -0500

    links.php added to view a list of users with details button for viewing details about that user

--- _Views/links.php
+++ _Views/links.php
@@ -6,11 +6,8 @@ foreach($data as $d){
 	echo $d["first_name"];
 	echo " ";
 	echo $d["last_name"];
-        echo "<br>";
 	echo " <a href=?action=details&id=".$d["id"]." class='btn'>details</a>";
-        echo " <a href=?action=update&id=".$d["id"]." class='btn alert-success'>update</a>";
-        echo " <a href=?action=delete&id=".$d["id"]." class='btn alert-danger'>delete</a>";
-	echo "<br><br>";
+	echo "<br>";
 }
 
 	
