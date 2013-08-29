c8795fc14b467e25c90185c8f869486cc9aee8d7
Robert Geiger <rjgeiger@me.com>
2013-08-29 12:57:49 -0500

    updated footer view with copyright information

--- _Views/footer.php
+++ _Views/footer.php
@@ -2,9 +2,4 @@
 	<small>© Copyright 2013, Leading Light Star</small>
 </footer>
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-wip/js/bootstrap.min.js"></script>
-<script src="_Libraries/_js/respond.min.js"></script>
-</body>
-</html>
+
